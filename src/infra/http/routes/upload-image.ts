492277c19018145246cfafedc2f8dc41da69e8fa
Poline Lottin <poline.lottin@goco.io>
2025-06-11 09:46:40 -0300

    fix: versions conflict

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -2,7 +2,30 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-	server.post('/uploads', async (request, reply) => {
-		return reply.status(200).send({ uploadId: 'test' })
-	})
+	server.post(
+		'/uploads',
+		{
+			schema: {
+				summary: 'Upload an image',
+				description: 'Upload an image to the server',
+				body: z.object({
+					name: z.string(),
+					password: z.string().optional(),
+				}),
+				response: {
+					201: z.object({
+						uploadId: z.string(),
+					}),
+					409: z
+						.object({
+							message: z.string(),
+						})
+						.describe('Upload already exists'),
+				},
+			},
+		},
+		async (request, reply) => {
+			return reply.status(200).send({ uploadId: 'test' })
+		}
+	)
 }
