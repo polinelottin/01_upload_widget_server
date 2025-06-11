b3e114d125024eec10eaac1554a91c8e7e4e5147
Poline Lottin <poline.lottin@goco.io>
2025-06-11 09:14:02 -0300

    chore: register swagger

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,10 +1,8 @@
-import type { FastifyInstance } from 'fastify'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-	server.post('/uploads', () => {
-		return {
-			message: 'Hello World',
-		}
+	server.post('/uploads', async (request, reply) => {
+		return reply.status(200).send({ uploadId: 'test' })
 	})
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,8 +1,12 @@
 import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
 	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
 	serializerCompiler,
 	validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -26,10 +30,29 @@ server.setErrorHandler((error, request, reply) => {
 	reply.status(500).send({ message: 'Internal server error' })
 })
 
-server.register(uploadImageRoute)
 server.register(fastifyCors, {
 	origin: '*',
 })
+
+// PLUGINS
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Upload Server',
+			description: 'API for uploading images',
+			version: '1.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
+
+// ROUTES
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('Server is running!')
